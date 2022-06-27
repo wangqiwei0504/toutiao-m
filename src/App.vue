5d1f5a4bd6b1c33098be6b35865c7dbf9d28ede5
wangqiwei0504 <1046005995@qq.com>
2022-06-27 21:33:43 +0800

    引入van组件库

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
-<div>
+  <div>
     <h1>黑马头条</h1>
-</div>
+    <div>
+      <i class="toutiao toutiao-shouji"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
 export default {
-  created () {},
+  created () { },
   data () {
     return {}
   },
@@ -19,5 +27,4 @@ export default {
 </script>
 
 <style scoped lang='less'>
-
 </style>
